431cf8ae3e2279bc8335a2700453477904a29d7d
kkapsner <github@mail.kkapsner.de>
2014-04-24 21:01:23 +0200

    Highly reduced kkjs.Debug
    most of the functionality is no longer needed in newer Browsers or is
    redundant.

--- modules/kkjs.Debug.js
+++ modules/kkjs.Debug.js
@@ -9,32 +9,18 @@
  * @description: sparsame Variante von Debug
  */
 
-function createMessageReceiver(callbackFn){
-	var rec = function(message){
-		try{
-			rec.messages.push(message);
-			rec.clearMessageStack();
-			callbackFn(message);
-		}
-		catch(e){
-			rec.messageStack.push(message);
-		}
-	};
-	rec.messages = [];
-	rec.messageStack = [];
-	rec.clearMessageStack = function(){
-		for (var i = 0; i < rec.messageStack.length; i++){
-			callbackFn(rec.messageStack[i]);
-		}
-	};
-	return rec;
-}
-
 var Debug = {
-	getFunctionName: function(func){
-		return (("name" in func)? func.name: func.toString().match(/^\s*function(?:\s+([^\(\)]+))?\s*\(/)[1]) || "{anonymous}";
-	},
 	getCallingStack: function getCallingStack(maxDepth){
+		/**
+		 * Function Debug.getCallingStack
+		 * @name: Debug.getCallingStack
+		 * @author: Korbinian Kapsner
+		 * @description: generates a calling stack.
+		 * @parameter:
+		 *	maxDepth: the maximal search depth
+		 * @return value: the calling stack
+		 */
+		
 		if (!maxDepth){
 			maxDepth = 10;
 		}
@@ -47,10 +33,20 @@ var Debug = {
 		}
 		return stack;
 	},
-
-	status: true,
 	
 	getElements: function(obj, re, wre){
+		/**
+		 * Function Debug.getElements
+		 * @name: Debug.getElements
+		 * @author: Korbinian Kapsner
+		 * @description: generates a overview of an object
+		 * @parameter:
+		 *	obj: the object to inspect
+		 *	re (optional): regular expression that the name has to match
+		 *	wre (optional): regular expressen that the value has to match
+		 * @return value: the generated object overview string
+		 */
+		
 		if (!re){
 			re = /./;
 		}
@@ -88,106 +84,25 @@ var Debug = {
 		return text;
 	},
 	
-	alert: function(str){
-		if (this.status){
-			if (/\n/.test(str)){
-				alert(str);
-			}
-			else{
-				window.status = "Bereit";
-				if (window.status === "Bereit"){
-					window.status = str;
-				}
-				else{
-					alert(str);
-				}
-			}
-			return true;
-		}
-		else {
-			window.status = "Debug.alert(" + str + ")";
-		}
-		return false;
-	},
-	
-	print_r: function(obj, depth){
-		if (!depth || isNaN(depth)){
-			depth = 5;
-		}
-		switch (typeof obj){
-			case "function":
-				return "function(){ ... }";
-			case "number":
-				return obj.toString(10);
-			case "string":
-				return JSON.stringify(obj);
-			case "undefined":
-				return "undefined";
-			case "boolean":
-				return obj.toString();
-			case "object":
-				if (obj === null){
-					return "null";
-				}
-				else if (Array.isArray(obj)){
-					if (!depth){
-						return "[ ... ]";
-					}
-					var ret = "[\n\t";
-					obj.forEach(function(el, i){
-						ret += Debug.print_r(el, depth - 1).replace(/\n/g, "\n\t");
-						if (i !== obj.length - 1){
-							ret += ",\n\t";
-						}
-					});
-					return ret + "\n]";
-				}
-				else {
-					if (!depth){
-						return "{ ... }";
-					}
-					var ret = [];
-					for (var i in obj){
-						ret.push(Debug.print_r(i) + ": " + Debug.print_r(obj[i], depth - 1).replace(/\n/g, "\n\t"));
-					}
-					return "{\n\t" + ret.join("\n\t") + "\n}";
-				}
-		}
-		return "";
-	},
-	
-	elementAlert : function(obj,re, wre){
-		return this.alert(this.getElements(obj,re, wre));
-	},
-	
 	elementAlertWin: function(obj, re, wre){
+		/**
+		 * Function Debug.elementAlertWin
+		 * @name: Debug.elementAlertWin
+		 * @author: Korbinian Kapsner
+		 * @description: opens a new window and outputs the result of
+		 *	Debug.getElements() there. Same parameter than Debug.getElements().
+		 * @parameter:
+		 *	obj:
+		 *	re:
+		 *	wre: 
+		 */
+		
 		var win = window.open("", "_blank");
 		var doc = win.document;
 		doc.write("<!DocType html><html><head><title>Elementausgabe</title></head><body><pre id='alert'></pre></body></html>");
 		doc.close();
 		doc.getElementById("alert").appendChild(doc.createTextNode(this.getElements(obj, re, wre)));
-	},
-	
-	errorList: ["Unbekannter Fehler", "Ihr Browser unterstützt diese Funktion nicht."],
-	
-	error: function(errorNumber){
-		var name = this.getFunctionName(Debug.error.caller);
-		
-		if (typeof errorNumber !== "string"){
-			if (typeof errorNumber !== "number" || errorNumber >= this.errorList.length){
-				errorNumber = 0;
-			}
-			errorNumber = this.errorList[errorNumber];
-		}
-		throw new Error(name + "() -> Error: " + errorNumber);
-	},
-	
-	warn: createMessageReceiver(function(warning){
-			console.warn(warning);
-	}),
-	info: createMessageReceiver(function(info){
-			console.info(info);
-	})
+	}
 };
 
 if (typeof exports !== "undefined"){
