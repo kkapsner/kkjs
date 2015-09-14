93f3c5aa17941d9fbd5eb52f1e9ccd619f88c92e
kkapsner <github@mail.kkapsner.de>
2015-09-14 12:47:52 +0200

    Added module kkjs.spyglass

---
+++ modules/kkjs.spyglass.js
@@ -0,0 +1,66 @@
+(function(){
+
+"use strict";
+
+/**
+ * Object spyglass
+ * @name: spyglass
+ * @author: Korbinian Kapsner
+ * @version: 1.0
+ * @description: spyglass description
+ */
+
+var spyglass = {
+	enable: function(img, attr){
+		var wrapper = kkjs.node.create({
+			tag: "span",
+			nextSibling: img,
+			className: "kkjs_spyglassWrapper"
+		 });
+		wrapper.appendChild(img);
+		var spy = kkjs.node.create({
+			tag: "span",
+			className: "spyglass",
+			parentNode: wrapper,
+			style: {
+				backgroundImage: "url(" + img.src + ")",
+				width: attr.size,
+				height: attr.size,
+				margin: attr.size / -2
+			}
+		});
+		kkjs.event.add(wrapper, ["mousemove", "mouseenter"], function(ev){
+			var size = new kkjs.Math.Dimension(spy.offsetWidth, spy.offsetHeight);
+			kkjs.css.set(spy, "display", "none");
+			var pos = kkjs.event.getMousePosition(ev).sub(kkjs.node.getPosition(wrapper));
+			var faktor = img.naturalWidth / img.width;
+			kkjs.css.set(spy, pos);
+			kkjs.css.set(spy, "backgroundPosition",
+				kkjs.sprintf("%dpx %dpx", size.width / 2 - pos.left * faktor, size.height / 2 - pos.top * faktor)
+			);
+			kkjs.css.set(spy, "display", "");
+		});
+	}.setDefaultParameter(null, new Function.DefaultParameter(
+		{
+			size: 200
+		},
+		{
+			checkType: true,
+			deepObjectInspection: true,
+			returnClone: true
+		}
+	))
+};
+
+if (typeof exports !== "undefined"){
+	for (var i in spyglass){
+		if (spyglass.hasOwnProperty(i)){
+			exports[i] = spyglass[i];
+		}
+	}
+}
+else if (typeof kkjs !== "undefined"){
+	kkjs.spyglass = spyglass;
+}
+
+})();
