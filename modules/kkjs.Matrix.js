6f2cde26d047ee0d2add0e6a938451981d67b14d
kkapsner <github@mail.kkapsner.de>
2018-10-07 10:47:12 +0200

    Improved matrix functions.

--- modules/kkjs.Matrix.js
+++ modules/kkjs.Matrix.js
@@ -12,7 +12,7 @@ function Matrix(height, width/*, initializer*/){
 		var colI = col * height;
 		for (var row = height - 1; row >= 0; row -= 1){
 			var i = colI + row;
-			storage[i] = initializer? initializer(row, col): 0;
+			storage[i] = initializer? initializer.call(this, row, col): 0;
 		}
 	}
 	this.get = function get(row, col){
@@ -50,11 +50,74 @@ Matrix.prototype.getSubMatrix = function matrixGetSubMatrix(rowOffset, colOffset
 		return original.get(row, col);
 	};
 	subMatrix.set = function set(row, col, value){
-		return original.set(row + rowOffset, col + colOffset, value);
+		throw new TypeError("Set operation not allowed on matrix view.");
 	}
 	
 	return subMatrix;
-	// return new SubMatrix(this, row, col, height, width, defaultValue);
+};
+
+Matrix.prototype.getTransposedMatrix = function matrixGetTransposedMatrix(){
+	var original = this;
+	var trMatrix = new Matrix(0, 0);
+	trMatrix.height = this.width;
+	trMatrix.width = this.height;
+	
+	trMatrix.get = function get(row, col){
+		return original.get(col, row);
+	};
+	trMatrix.set = function set(row, col, value){
+		throw new TypeError("Set operation not allowed on matrix view.");
+	}
+	
+	return trMatrix;
+};
+
+Matrix.prototype.getAntiTransposedMatrix = function matrixGetAntiTransposedMatrix(){
+	var original = this;
+	var atrMatrix = new Matrix(0, 0);
+	atrMatrix.height = this.width;
+	atrMatrix.width = this.height;
+	
+	atrMatrix.get = function get(row, col){
+		return original.get(this.width - 1 - col, this.height - 1 - row);
+	};
+	atrMatrix.set = function set(row, col, value){
+		throw new TypeError("Set operation not allowed on matrix view.");
+	}
+	
+	return atrMatrix;
+};
+
+Matrix.prototype.getHorizontalFlippedMatrix = function matrixGetHorizontalFlippedMatrix(){
+	var original = this;
+	var hfMatrix = new Matrix(0, 0);
+	hfMatrix.height = this.height;
+	hfMatrix.width = this.width;
+	
+	hfMatrix.get = function get(row, col){
+		return original.get(row, this.width - 1 - col);
+	};
+	hfMatrix.set = function set(row, col, value){
+		throw new TypeError("Set operation not allowed on matrix view.");
+	}
+	
+	return hfMatrix;
+};
+
+Matrix.prototype.getVerticalFlippedMatrix = function matrixGetVerticalFlippedMatrix(){
+	var original = this;
+	var vfMatrix = new Matrix(0, 0);
+	vfMatrix.height = this.height;
+	vfMatrix.width = this.width;
+	
+	vfMatrix.get = function get(row, col){
+		return original.get(this.height - 1 - row, col);
+	};
+	vfMatrix.set = function set(row, col, value){
+		throw new TypeError("Set operation not allowed on matrix view.");
+	}
+	
+	return vfMatrix;
 };
 
 Matrix.prototype.getRow = function matrixGetRow(row){
@@ -214,7 +277,53 @@ Matrix.prototype.prod = function matrixProd(){
 	return this.reduce(function(a, b){return a * b;});
 };
 
-Matrix.prototype.toString = function matrixToString(){
+Matrix.prototype.conv = function matrixConv(conv/*, outputSize*/){
+	var heightChange;
+	var rowOffset;
+	var widthChange;
+	var colOffset;
+	var outputSize;
+	
+	if (arguments.length > 1){
+		outputSize = arguments[1];
+	}
+	else {
+		outputSize = "max";
+	}
+	switch (outputSize){
+		case "valid":
+			heightChange = 1 - conv.height;
+			rowOffset = 0;
+			widthChange = 1 - conv.width;
+			colOffset = 0;
+			break;
+		case "original":
+			heightChange = 0;
+			rowOffset = -Math.floor((conv.height - 1) / 2);
+			widthChange = 0;
+			colOffset = -Math.floor((conv.width - 1) / 2);
+			break;
+		default:
+			heightChange = conv.height - 1;
+			rowOffset = -heightChange;
+			widthChange = conv.width - 1;
+			colOffset = -widthChange;
+			break;
+	}
+	
+	var This = this;
+	return new Matrix(
+		this.height + heightChange,
+		this.width + widthChange,
+		function(row, col){
+			var subMatrix = This.getSubMatrix(row + rowOffset, col + colOffset, conv.height, conv.width, 0);
+			var mul = Matrix.mul(subMatrix, conv);
+			return mul.sum();
+		}
+	);
+};
+
+Matrix.prototype.join = function matrixJoin(rowJoin, colJoin){
 	var rows = [];
 	this.forEachRow(function(row){
 		rows.push(
@@ -224,10 +333,15 @@ Matrix.prototype.toString = function matrixToString(){
 					return col;
 				},
 				[]
-			).join(", ")
+			).join(rowJoin)
 		);
 	});
-	return rows.join("\n");
+	return rows.join(colJoin);
+	
+};
+
+Matrix.prototype.toString = function matrixToString(){
+	this.join(", ", "\n");
 };
 
 Matrix.prototype.toArray = function matrixToArray(){
@@ -244,7 +358,14 @@ Matrix.prototype.toArray = function matrixToArray(){
 		);
 	});
 	return rows;
-}
+};
+
+Matrix.prototype.clone = function matrixClone(){
+	var This = this;
+	return new Matrix(this.height, this.width, function(row, col){
+		return This.get(row, col);
+	});
+};
 
 Matrix.fromArray = function matrixFromArray(arr){
 	var height = arr.length;
@@ -289,6 +410,49 @@ Matrix.div = function matrixDiv(m1, m2){
 	return Matrix.combine(m1, m2, function(v1, v2){return v1 / v2;});
 };
 
+Matrix.concatHorizontal = function matrixConcatHorizontal(m1, m2){
+	if (m1.height !== m2.height){
+		throw new Error("Missmatching dimensions");
+	}
+	
+	return new Matrix(m1.height, m1.width + m2.width, function(row, col){
+		if (col < m1.width){
+			return m1.get(row, col);
+		}
+		else {
+			return m2.get(row, col - m1.width);
+		}
+	});
+};
+
+Matrix.concatVertical = function matrixConcatVertical(m1, m2){
+	if (m1.width !== m2.width){
+		throw new Error("Missmatching dimensions");
+	}
+	
+	return new Matrix(m1.height + m2.height, m1.width, function(row, col){
+		if (row < m1.height){
+			return m1.get(row, col);
+		}
+		else {
+			return m2.get(row - m1.height, col);
+		}
+	});
+};
+
+Matrix.prod = function matrixProduct(m1, m2){
+	if (m1.width !== m2.height){
+		throw new Error("Missmatching dimensions");
+	}
+	
+	return new Matrix(m1.height, m2.width, function(row, col){
+		var sum = 0;
+		for (var i = m1.width - 1; i >= 0; i -= 1){
+			sum += m1.get(row, i) * m2.get(i, col);
+		}
+		return sum;
+	});
+};
 
 if (typeof exports !== "undefined"){
 	module.exports = Matrix;
